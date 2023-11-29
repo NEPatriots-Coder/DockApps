903fab1b191e36a1c218e07835c31fee56ea9f4c
NEPatriots-Coder <144710540+NEPatriots-Coder@users.noreply.github.com>
2023-11-28 22:45:38 -0600

    Update ruan_11.27.py
    Updated file path

--- ruan_11.27.py
+++ ruan_11.27.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import streamlit as st
+import os
+
+csv_file_path = (r"C:\Users\lamarwells\Documents\DockApps\Trailer_count-11.27-3.csv")
+
+
+script_dir = os.path.dirname(os.path.abspath(csv_file_path))
+
+
+file_path = os.path.join(script_dir, "Trailer_count-11.27-3.csv")
 
 # Force Streamlit to rerun in "report mode"
 
@@ -8,7 +17,7 @@ st.title("Inbound From Ruan")
 
 
 
-df = pd.read_csv('C:\\Users\\lamarwells\\OneDrive - Doosan\\GWN_APPS\\DockApps\\Trailer_count-11.27-3.csv')
+df = pd.read_csv(file_path)
 
 st.write("## Trailer Count Wednesday 11-27-2023")
 st.write(df)
