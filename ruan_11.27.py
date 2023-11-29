662c8f2e3683edb74fdde4f97c4c5b9802e53dea
NEPatriots-Coder <144710540+NEPatriots-Coder@users.noreply.github.com>
2023-11-28 22:02:24 -0600

    First Commit
    Daily Dashboard

---
+++ ruan_11.27.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+
+# Force Streamlit to rerun in "report mode"
+
+
+st.title("Inbound From Ruan")
+
+
+
+df = pd.read_csv('Trailer_count-11.27-3.csv')
+
+st.write("## Trailer Count Wednesday 11-27-2023")
+st.write(df)
